280bf2aed5f3c9fb72e4504825daedaaa46aa779
Johnson Masino <johnsonmasino@gmail.com>
2023-10-06 09:09:22 +0100

    Pushing the latest changes

--- base/views.py
+++ base/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def Home(request):
     context = {}
-    return render(request, 'letstalk/navbar.html', context)
+    return render(request, 'letstalk/main.html', context)
